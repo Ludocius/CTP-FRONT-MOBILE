04a91704cc18fafadd5d0fc5e5385e9773b9efbb
Luis Fernando <luciusfernatore@outlook.com>
2021-03-05 18:26:06 -0500

    Wireframe: ready to enhance and make functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
-//Folders Created
-//just a hi!!
+//Wireframe 0.0.1
+//Author: Ludocius
+//TODO
+//Login
+//
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Scaffold(
-          body:Center(
-            child: Text(
-              'Hello mentores!'
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent.shade100,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(20.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+              ),
             ),
-          ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Card(
+              margin: EdgeInsets.all(20.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'password',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            RaisedButton(
+              disabledColor: Colors.white,
+              child: Text(
+                "LOG IN",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
-
-
-
